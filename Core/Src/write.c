b64e80661e96ce2ba74898a94a9875a3151623dd
Straviaz <joy.sangsrichan@gmail.com>
2020-11-08 00:52:15 -0500

    🤞

--- Core/Src/write.c
+++ Core/Src/write.c
@@ -5,10 +5,10 @@
 #include "main.h"
 #include <stdint.h>
 
-extern UART_HandleTypeDef huart2;
+extern UART_HandleTypeDef huart1;
 
 int _write(int file, char *ptr, int len) {
     // ignore the file, I guess
-    HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, 10);
+    HAL_UART_Transmit(&huart1, (uint8_t*)ptr, len, 10);
     return len;
 }

89d3e9280958658fe141a2e0987449bcb90f2db3
WillMerges <wdmerges@gmail.com>
2020-11-14 10:00:15 -0500

    read imu whoami

--- Core/Src/init.cpp
+++ Core/Src/init.cpp
@@ -27,34 +27,52 @@ void LED_loop() {
     HAL_GPIO_TogglePin(LEDPorts[i], LEDPins[i]);
 }
 
+#define IMU_ADDR 0b11010111
+#define IMU_WHOAMI 0x0F
+#define IMU_MAG_ADDR 0b00111011
+
 int init(void) {
-    while (1) {
-      printf("Starting...\r\n");
-
-      MPL3115A2 mpl;
-      bool ret = mpl.begin(&hi2c1);
-      if(!ret) {
-          printf("Altimeter failed to begin.\r\n");
-          return 0;
-      }
-
-      //mpl.setSeaPressure(101325);
-      float alt = -1;
-      float temp = -1;
-
-      HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
-
-      while(1) {
-          LED_loop();
-
-          alt = mpl.getAltitude()*3.28084;
-          printf("Altitude: %f ft\r\n", alt);
-
-          temp = mpl.getTemperature();
-          printf("Temperature: %f degrees C\r\n", temp);
-
-          HAL_Delay(500);
-      }
-      return 0;
+    uint8_t buff[1];
+    uint16_t send[1];
+    send[0] = IMU_WHOAMI;
+    while(1) {
+        HAL_I2C_Mem_Read(&hi2c1, IMU_ADDR, *send, 1, buff, 1, 200);
+        printf("imu whoami: 0x%0x\r\n", buff[0]);
+        HAL_I2C_Mem_Read(&hi2c1, IMU_MAG_ADDR, *send, 1, buff, 1, 200);
+        printf("mag whoami: 0x%0x\r\n", buff[0]);
+        HAL_Delay(500);
     }
+    return 0;
 }
+
+// int init(void) {
+//     while (1) {
+//       printf("Starting...\r\n");
+//
+//       MPL3115A2 mpl;
+//       bool ret = mpl.begin(&hi2c1);
+//       if(!ret) {
+//           printf("Altimeter failed to begin.\r\n");
+//           return 0;
+//       }
+//
+//       //mpl.setSeaPressure(101325);
+//       float alt = -1;
+//       float temp = -1;
+//
+//       HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+//
+//       while(1) {
+//           LED_loop();
+//
+//           alt = mpl.getAltitude()*3.28084;
+//           printf("Altitude: %f ft\r\n", alt);
+//
+//           temp = mpl.getTemperature();
+//           printf("Temperature: %f degrees C\r\n", temp);
+//
+//           HAL_Delay(500);
+//       }
+//       return 0;
+//     }
+//}

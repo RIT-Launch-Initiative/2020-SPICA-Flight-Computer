ce1fdf8f4188554655cf6b8cfd768827432a47c6
WillMerges <wdmerges@gmail.com>
2021-03-24 20:29:10 -0400

    small cleanup

--- Core/Src/init.cpp
+++ Core/Src/init.cpp
@@ -27,19 +27,11 @@ void test() {
     }
 }
 
-extern UART_HandleTypeDef huart1;
 int init() {
     HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
 
-    // MTK3339::init(NMEA_OUTPUT_DEFAULT, NMEA_RATE_DEFAULT);
-    // if(!W25qxx_Init()) {
-    //     printf("failed to init spi flash\r\n");
-    //     return -1;
-    // }
-
-    // test();
-
     if(sys_init() < 0) {
+        extern UART_HandleTypeDef huart1;
         HAL_UART_Transmit(&huart1, (uint8_t*)"error\r\n", 8, 100);
     }
 
@@ -68,45 +60,3 @@ int init() {
 
     return -1; // should never return
 }
-
-
-// int init(void) {
-//
-// #ifdef DEBUG
-//     HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
-//     while(1) {
-//         // chkhw();
-//         LED_loop();
-//     }
-// #endif
-//
-//     while (1) {
-//       printf("Starting...\r\n");
-//
-//       MPL3115A2 mpl;
-//       bool ret = mpl.begin(&hi2c1);
-//       if(!ret) {
-//           printf("Altimeter failed to begin.\r\n");
-//           return 0;
-//       }
-//
-//       //mpl.setSeaPressure(101325);
-//       float alt = -1;
-//       float temp = -1;
-//
-//       HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
-//
-//       while(1) {
-//           LED_loop();
-//
-//           alt = mpl.getAltitude()*3.28084;
-//           printf("Altitude: %f ft\r\n", alt);
-//
-//           temp = mpl.getTemperature();
-//           printf("Temperature: %f degrees C\r\n", temp);
-//
-//           HAL_Delay(500);
-//       }
-//       return 0;
-//     }
-// }

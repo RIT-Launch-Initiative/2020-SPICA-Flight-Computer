330e7eaa944da05db2eb5bd3a680bfea70713911
WillMerges <wdmerges@gmail.com>
2021-03-31 21:40:14 -0400

    gps interrupt mode working

--- Core/Src/gps.c
+++ Core/Src/gps.c
@@ -36,7 +36,7 @@ void gps_init() {
     gps_task.start_time = ts_systime();
     gps_task.priority = SLEEP_PRIORITY;
     gps_task.task = &gps_update;
-    init_gga(NULL, NULL);
+    init_gga(NMEA_OUTPUT_DEFAULT, NMEA_RATE_DEFAULT);
 
     // this has to be the last thing init does
     // TODO maybe return if this isn't HAL_OK
@@ -47,7 +47,6 @@ void gps_init() {
 
 // TODO check for errors on return with memcpyout
 void gps_update(tiny_task_t* task) {
-    printf("gps update\r\n");
     disable_irq;
     lines--; // event is being handled
     enable_irq;
@@ -65,16 +64,14 @@ void gps_update(tiny_task_t* task) {
         i++;
     }
 
-    // TODO
-    // parse out buff into usable data
-    // parse_gga((char*)buff, &gps_packet, i);
-
     #ifdef DEBUG
     buff[i] = '\0';
     printf("%s\n", buff);
-    // printf("time: %s, lat: %i.%f, long: %i.%f, alt: %f, fix: %i, sats: %i", gps_packet.time,
-            // gps_packet.latitude.degrees, gps_packet.latitude.minutes, gps_packet.longitude.degrees,
-            // gps_packet.longitude.minutes, gps_packet.altitude, gps_packet.fix, gps_packet.sat_count);
+
+    parse_gga((char*)buff, &gps_packet, i);
+    printf("time: %s, lat: %i.%f, long: %i.%f, alt: %f, fix: %i, sats: %i\r\n\r\n", gps_packet.time,
+            gps_packet.latitude.degrees, gps_packet.latitude.minutes, gps_packet.longitude.degrees,
+            gps_packet.longitude.minutes, gps_packet.altitude, gps_packet.fix, gps_packet.sat_count);
     #endif
 
     if(!lines) { // check if we have more lines to process
@@ -89,6 +86,7 @@ void gps_RxCallback() {
     if(gps_char == GPS_ENDL) {
         lines++; // we have another line to handle!
         gps_task.priority = LOW_PRIORITY; // TODO maybe make this dynamic
+        gps_task.start_time = ts_systime(); // this may be unnecessary
     }
 
     HAL_UART_Receive_IT(&GPS_UART, &gps_char, 1);

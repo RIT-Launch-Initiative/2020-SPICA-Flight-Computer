3bdd7887c466425b42bf7dea7ed0321cf77d06f4
WillMerges <wdmerges@gmail.com>
2021-04-15 21:28:12 -0400

    added header to logger

--- Core/Src/log.c
+++ Core/Src/log.c
@@ -3,6 +3,7 @@
 */
 #include "log.h"
 #include <string.h> // memset
+#include <stdio.h>
 #include "lib/common/common.h" // _write
 
 // XBEE UART file descriptor (as in lib/sys.c)
@@ -11,12 +12,22 @@
 tiny_task_t log_task;
 log_packet_t log_packet;
 
+typedef struct {
+    uint8_t preamble[4]; // ">>>>"
+    uint32_t payload_size;
+} header_t;
+
+header_t header;
 
 // function called in the log task
 void log_update(tiny_task_t* task) {
     log_packet.uptime = ts_systime();
-
+    _write(XBEE_FD, (char*)&header, sizeof(header_t));
     _write(XBEE_FD, (char*)&log_packet, sizeof(log_packet_t));
+
+    #ifdef DEBUG
+    printf("logged packet\r\n");
+    #endif
 
     task->start_time += LOG_PERIOD;
 }
@@ -27,6 +38,13 @@ void log_init() {
     log_task.start_time = ts_systime() + LOG_PERIOD;
     log_task.default_priority = MEDIUM_PRIORITY; // higher priority than collecting data
     log_task.task = &log_update;
+
+    // setup header
+    header.preamble[0] = '>';
+    header.preamble[1] = '>';
+    header.preamble[2] = '>';
+    header.preamble[3] = '>';
+    header.payload_size = sizeof(log_packet_t);
 
     // zero the packet
     memset(&log_packet, 0, sizeof(log_packet_t));
